0a7e399edd9e7af73c2907feb39075cbfb8850f0
Olivier JM Maniraho <manolivier93@gmail.com>
2024-03-12 12:04:31 +0200

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Analytics } from '@vercel/analytics/react';
 import { Router } from './Router';
 import { theme } from './theme';
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Router />
+      <Analytics />
     </MantineProvider>
   );
 }
