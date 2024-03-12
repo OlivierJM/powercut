a77f16e70960136a751d2ff9afe0d5c7d233b03a
Olivier JM Maniraho <manolivier93@gmail.com>
2024-03-12 09:39:46 +0200

    add images and logo

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: 'autoUpdate' })],
   test: {
     globals: true,
     environment: 'jsdom',
