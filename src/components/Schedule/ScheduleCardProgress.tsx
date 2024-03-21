60b0114c06f6f2f90f2a08d777db613676c24c35
mwila <mpnyirongo@gmail.com>
2024-03-21 15:49:12 +0200

    feat: created progress tests for `ScheduleCardProgress`

--- src/components/Schedule/ScheduleCardProgress.tsx
+++ src/components/Schedule/ScheduleCardProgress.tsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Progress, Transition } from '@mantine/core';
 
-const ScheduleCardProgess = ({ value }: { value: number }) => {
+const ScheduleCardProgress = ({ value }: { value: number }) => {
+  const [mounted, setMounted] = useState(false)
   const [progressValue, setProgressValue] = useState(0);
   useEffect(() => {
     setTimeout(() => {
+      setMounted(true)
       setProgressValue(value);
     }, 100);
     return () => {
+      setMounted(false);
       setProgressValue(0);
     };
   }, []);
 
   return (
     <Transition
-      mounted={progressValue !== 0}
+      mounted={mounted}
       transition="scale-x"
       duration={700}
       timingFunction="ease"
@@ -35,4 +38,4 @@ const ScheduleCardProgess = ({ value }: { value: number }) => {
   );
 };
 
-export default ScheduleCardProgess;
+export default ScheduleCardProgress;
